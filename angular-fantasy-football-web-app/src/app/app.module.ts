ec4082c460e478840ecd28c5ac780a32bf6c75da
namyrrepa <perryman.austinm@gmail.com>
2022-09-08 13:48:01 -0400

    Changed ff-tools component to just tools

--- angular-fantasy-football-web-app/src/app/app.module.ts
+++ angular-fantasy-football-web-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
